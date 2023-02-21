45223aea184b8d59c9737e23aee626a97fcb7358
zhaogang9662 <wangyiemailz@126.com>
2023-02-22 00:51:23 +0800

    feat: 修改产物路径

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "docs",
+  },
   plugins: [
     vue(),
     AutoImport({
